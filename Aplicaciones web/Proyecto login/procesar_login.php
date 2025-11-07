953c9f732f290b1ca7cefab2ab438462414d08ed
Pokemiri <sailormiri11@gmail.com>
2025-11-07 09:45:14 -0500

    Cambio

--- Aplicaciones web/Proyecto login/procesar_login.php
+++ Aplicaciones web/Proyecto login/procesar_login.php
@@ -4,22 +4,22 @@ session_start();
 $usuario = $_POST['usuario'];
 $password = $_POST['password'];
 
-$usuario = file("usuarios.txt", FILE_IGNORE_NEW_LINES);
+$usuarios = file("usuarios.txt", FILE_IGNORE_NEW_LINES);
 
-$login_existoso = false;
-foreach ($usuario as $linea) {
-    list($user, $hash) = explode(":", $linea);
-    if ($user === $usuario && password_verify($password, $hash)) {
-        $login_existoso = true;
+$login_exitoso = false;
+foreach ($usuarios as $linea) {
+     list($user, $hash) = explode(":", $linea);
+     if ($user === $usuario && password_verify($password, $hash)) {
+        $login_exitoso = true;
         $_SESSION['usuario'] = $usuario;
         break;
-    }
+     }
 }
-if($login_existoso){
-    header("Location: bienvenida.php");
-    exit;
+if($login_exitoso) {
+     header("Location: bienvenida.php");
+     exit;
 }else{
-    echo "<h1>Usuario o contraseña incorrectos</h1>";
-    echo "<p><a href='login.php'>Volver a intentar</a></p>";
+     echo "<h1>Usuario o contraseña incorrectos</h1>";
+     echo "<p><a href='login.php'>Volver a intentar</a></p>";
 }
 ?>
