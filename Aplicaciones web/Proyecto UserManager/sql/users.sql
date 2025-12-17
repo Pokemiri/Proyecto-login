db3f73dafc88a4b2d18538965b37071df57b40c1
Pokemiri <sailormiri11@gmail.com>
2025-12-17 13:42:19 -0500

    Modificacion al archivo users.sql

--- Aplicaciones web/Proyecto UserManager/sql/users.sql
+++ Aplicaciones web/Proyecto UserManager/sql/users.sql
@@ -3,9 +3,13 @@ USE usermanager_loginYregistro;
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
     usuario VARCHAR(50) NOT NULL UNIQUE,
     contrasena VARCHAR(255) NOT NULL,
+);
+CREATE TABLE perfil (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id VARCHAR(50) NOT NULL UNIQUE,
+    nombre VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     edad INT NOT NULL,
     rol ENUM('admin','user') NOT NULL
